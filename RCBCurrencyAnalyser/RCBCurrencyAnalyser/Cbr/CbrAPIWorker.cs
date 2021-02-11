a7bc83bac60913971f476fc992defa45bf9c3bc7
BarefootElf <AndreiRoot@protonmail.ch>
2021-02-11 22:24:43 +0300

    Added CbrAPIWorker

---
+++ RCBCurrencyAnalyser/RCBCurrencyAnalyser/Cbr/CbrAPIWorker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+
+namespace GMCS_CurrencyAnalyzer {
+    public static class CbrAPIWorker {
+        public static XmlDocument GetCurrencyCatalog(bool daily) {
+            var webRequest = WebRequest.Create("http://www.cbr.ru/scripts/XML_val.asp?d=" + (daily ? "0" : "1"));
+            HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+            string xmlText = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(1251)).ReadToEnd();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlText);
+            return xmlDoc;
+        }
+
+        public static XmlDocument GetCurrencyDaily(DateTime date) {
+            var webRequest = WebRequest.Create("http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + date.Date.ToString("dd'/'MM'/'yyyy"));
+            HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+            string xmlText = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(1251)).ReadToEnd();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlText);
+            return xmlDoc;
+        }
+    }
+}
